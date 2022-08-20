9481c4e12754c5da13a0d12ba66de5a77fe0f01e
Ilyasse OUADDENE <LiveZizoo.maag@gmail.com>
2022-08-20 22:06:05 +0100

    mod01

--- WindowsFormsApplication2/Form1.Designer.cs
+++ WindowsFormsApplication2/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(711, 12);
+            this.button1.Location = new System.Drawing.Point(416, 12);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(230, 59);
             this.button1.TabIndex = 0;
@@ -54,7 +54,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(953, 499);
+            this.ClientSize = new System.Drawing.Size(830, 333);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
