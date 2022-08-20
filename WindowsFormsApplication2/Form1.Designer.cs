b4d5f1722f22eea12581a8999833578cbca04215
Ilyasse OUADDENE <LiveZizoo.maag@gmail.com>
2022-08-20 22:10:19 +0100

    mod2

--- WindowsFormsApplication2/Form1.Designer.cs
+++ WindowsFormsApplication2/Form1.Designer.cs
@@ -28,18 +28,8 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(416, 12);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(230, 59);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
@@ -54,9 +44,8 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(830, 333);
+            this.ClientSize = new System.Drawing.Size(873, 368);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -64,8 +53,6 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
     }
 }
